36bab97544c24f9ac3681f0e939bbc9357e7fecd
tingyuan <tingyuan@iflytek.com>
2022-09-16 14:07:49 +0800

    feat:develop feature

--- similarimageplugin/src/main/java/com/similarimage/tools/CheckImageSimilarAction.java
+++ similarimageplugin/src/main/java/com/similarimage/tools/CheckImageSimilarAction.java
@@ -1,13 +1,9 @@
 package com.similarimage.tools;
-
-import com.fasterxml.jackson.jr.ob.JSON;
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-import kotlinx.serialization.json.Json;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,20 +17,15 @@ public class CheckImageSimilarAction extends AnAction {
         Project project = e.getData(PlatformDataKeys.PROJECT);
 
         String text = Messages.showInputDialog(project,
-                "请输入待检测图片的文件相对路径",
+                "请输入待检测文件的绝对路径",
                 "",
                 Messages.getQuestionIcon());
         HashMap<String, ArrayList<String>> result = SimilarImageAlgorithm.checkImage(text);
-        writeFile(text, result, e);
-//        Messages.showMessageDialog(project,
-//                "What you have entered is:" + text,
-//                "Information",
-//                Messages.getInformationIcon());
+        writeFile(text, result);
     }
 
-    private void writeFile (String path, HashMap<String, ArrayList<String>> result, AnActionEvent event) {
-        String filePath = "/Users/codoon/Documents/project/SomeTest" + File.separator + System.currentTimeMillis()  + ".txt";
-        System.out.println(filePath);
+    private void writeFile (String path, HashMap<String, ArrayList<String>> result) {
+        String filePath = path + File.separator + System.currentTimeMillis()  + ".txt";
         StringBuilder content = new StringBuilder();
         if (result != null) {
             for (String key : result.keySet()) {
@@ -49,12 +40,6 @@ public class CheckImageSimilarAction extends AnAction {
                 }
             }
         }
-//        Project project = event.getData(PlatformDataKeys.PROJECT);
-//                Messages.showMessageDialog(project,
-//                content.toString(),
-//                "result",
-//                Messages.getInformationIcon());
-        System.out.println(content.toString());
         FileWriter fw = null;
         try {
             File file = new File(filePath);
@@ -67,6 +52,7 @@ public class CheckImageSimilarAction extends AnAction {
             e.printStackTrace();
         } finally {
             try {
+                assert fw != null;
                 fw.close();
             }
             catch (Exception e) {
--- similarimageplugin/src/main/java/com/similarimage/tools/SimilarImageAlgorithm.java
+++ similarimageplugin/src/main/java/com/similarimage/tools/SimilarImageAlgorithm.java
@@ -1,5 +1,7 @@
 package com.similarimage.tools;
 
+import com.intellij.util.ui.UIUtil;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.color.ColorSpace;
@@ -17,10 +19,8 @@ import java.util.List;
  * @date: 2022/9/13
  */
 public class SimilarImageAlgorithm {
-
-    private static final String TAG = SimilarImageAlgorithm.class.getSimpleName();
+    public static List<ImageInfo> imageInfoList = new ArrayList<>();
     private static int threshold = 95;
-
     public static void main(String[] args) {
 
     }
@@ -52,25 +52,23 @@ public class SimilarImageAlgorithm {
         return result;
     }
 
-    public static List<ImageInfo> imageInfoList = new ArrayList<>();
-
     public static void tree(File f, int level) {
         File[] childArrays = f.listFiles();
         if (childArrays == null || childArrays.length == 0) {
             return;
         }
-        for (int i = 0; i < childArrays.length; i++) {
-            if (childArrays[i].isDirectory()) {
-                tree(childArrays[i], level + 1);
+        for (File childArray : childArrays) {
+            if (childArray.isDirectory()) {
+                tree(childArray, level + 1);
             } else {
-                String filename = childArrays[i].getName();
+                String filename = childArray.getName();
                 String fileSuffix = filename.substring(filename.lastIndexOf(".") + 1);
-                if ("jpg".equals(fileSuffix) || "png".equals(fileSuffix) || "webp".equals(fileSuffix)) {
+                if ("jpg".equals(fileSuffix) || "jpeg".equals(fileSuffix) || "png".equals(fileSuffix) || "webp".equals(fileSuffix)) {
                     ImageInfo imageInfo = new ImageInfo();
                     imageInfo.name = filename;
-                    imageInfo.path = childArrays[i].getPath();
+                    imageInfo.path = childArray.getPath();
                     try {
-                        imageInfo.fingerPrint = getImgFinger(childArrays[i]);
+                        imageInfo.fingerPrint = getImgFinger(childArray);
                         imageInfoList.add(imageInfo);
                     } catch (IOException e) {
                         System.out.println(e.getMessage());
@@ -82,23 +80,25 @@ public class SimilarImageAlgorithm {
 
     }
 
+    /**
+     * 对比两张图的获取相似度
+     */
     public static double getSimilarity(File imageFile1, File imageFile2) throws IOException {
         int[] pixels1 = getImgFinger(imageFile1);
         int[] pixels2 = getImgFinger(imageFile2);
-        // 获取两个图的汉明距离(假设另一个图也已经按上面步骤得到灰度比较数组)
         int hammingDistance = getHammingDistance(pixels1, pixels2);
-        // 通过汉明距离计算相似度，取值范围 [0.0, 1.0]
-        double similarity = calSimilarity(hammingDistance) * 100;
-        System.out.println("相似度:" + similarity + "%");
-        return similarity;
+        return calSimilarity(hammingDistance) * 100;
 
     }
 
+    /**
+     * 获取图片的指纹
+     */
     private static int[] getImgFinger(File imageFile) throws IOException {
         Image image = ImageIO.read(imageFile);
         // 转换至灰度
         image = toGrayscale(image);
-        // 缩小成32x32的缩略图
+        // 缩小成32x32的缩略图，这里也可以缩略成8*8
         image = scale(image);
         // 获取灰度像素数组
         int[] pixels1 = getPixels(image);
@@ -111,10 +111,7 @@ public class SimilarImageAlgorithm {
     }
 
     /**
-     * 将任意Image类型图像转换为BufferedImage类型，方便后续操作
-     *
-     * @param srcImage
-     * @return
+     * 将任意Image类型图像转换为BufferedImage类型
      */
     public static BufferedImage convertToBufferedFrom(Image srcImage) {
         BufferedImage bufferedImage = new BufferedImage(srcImage.getWidth(null),
@@ -128,9 +125,6 @@ public class SimilarImageAlgorithm {
 
     /**
      * 转换至灰度图
-     *
-     * @param image
-     * @return
      */
     public static BufferedImage toGrayscale(Image image) {
         BufferedImage sourceBuffered = convertToBufferedFrom(image);
@@ -143,9 +137,6 @@ public class SimilarImageAlgorithm {
 
     /**
      * 缩放至32x32像素缩略图
-     *
-     * @param image
-     * @return
      */
     public static Image scale(Image image) {
         image = image.getScaledInstance(32, 32, Image.SCALE_SMOOTH);
@@ -155,26 +146,18 @@ public class SimilarImageAlgorithm {
 
     /**
      * 获取像素数组
-     *
-     * @param image
-     * @return
      */
     public static int[] getPixels(Image image) {
         int width = image.getWidth(null);
         int height = image.getHeight(null);
-        int[] pixels = convertToBufferedFrom(image).getRGB(0, 0, width, height,
+        return convertToBufferedFrom(image).getRGB(0, 0, width, height,
                 null, 0, width);
-
-        return pixels;
 
     }
 
 
     /**
      * 获取灰度图的平均像素颜色值
-     *
-     * @param pixels
-     * @return
      */
     public static int getAverageOfPixelArray(int[] pixels) {
         Color color;
@@ -183,17 +166,12 @@ public class SimilarImageAlgorithm {
             color = new Color(pixels[i], true);
             sumRed += color.getRed();
         }
-        int averageRed = (int) (sumRed / pixels.length);
-        return averageRed;
+        return (int) (sumRed / pixels.length);
 
     }
 
     /**
      * 获取灰度图的像素比较数组(平均值的离差)
-     *
-     * @param pixels
-     * @param averageColor
-     * @return
      */
     public static int[] getPixelDeviateWeightsArray(int[] pixels, final int averageColor) {
         Color color;
@@ -212,11 +190,7 @@ public class SimilarImageAlgorithm {
     /**
      * 获取两个缩略图的平均像素比较数组的汉明距离(距离越大差异越大)
      *
-     * @param a
-     * @param b
-     * @return
      */
-
     public static int getHammingDistance(int[] a, int[] b) {
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -229,16 +203,12 @@ public class SimilarImageAlgorithm {
 
     /**
      * 通过汉明距离计算相似度
-     *
-     * @param hammingDistance
-     * @return
      */
     public static double calSimilarity(int hammingDistance) {
         int length = 32 * 32;
         double similarity = (length - hammingDistance) / (double) length;
-// 使用指数曲线调整相似度结果
+        // 使用指数曲线调整相似度结果
         similarity = java.lang.Math.pow(similarity, 2);
-
         return similarity;
 
     }
